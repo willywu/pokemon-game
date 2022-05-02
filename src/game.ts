b255a19c6292413e483561525a01f596e289cb36
Willy Wu <willy@doorstead.com>
2022-05-02 13:42:39 +0800

    Added pokemon images to home screen, ignored generated files

--- src/game.ts
+++ src/game.ts
@@ -1,47 +1,59 @@
-import 'phaser';
+import "phaser";
 
-export default class Demo extends Phaser.Scene
-{
-    constructor ()
-    {
-        super('demo');
-    }
+export default class Demo extends Phaser.Scene {
+  constructor() {
+    super("demo");
+  }
 
-    preload ()
-    {
-        this.load.image('logo', 'assets/phaser3-logo.png');
-        this.load.image('libs', 'assets/libs.png');
-        this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-        this.load.glsl('stars', 'assets/starfields.glsl.js');
-    }
+  preload() {
+    this.load.glsl("stars", "assets/starfields.glsl.js");
+    this.load.image("bulbasaur", "assets/pokemon/thumbnails/001.png");
+    this.load.image("ivysaur", "assets/pokemon/thumbnails/002.png");
+    this.load.image("venusaur", "assets/pokemon/thumbnails/003.png");
+  }
 
-    create ()
-    {
-        this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
+  create() {
+    this.add.shader("RGB Shift Field", 0, 0, 800, 600).setOrigin(0);
 
-        this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
+    const bulbasaurImage = this.add.image(200, 50, "bulbasaur");
+    const ivysaurImage = this.add.image(400, 50, "ivysaur");
+    const venusaurImage = this.add.image(600, 50, "venusaur");
 
-        this.add.image(400, 300, 'libs');
+    this.tweens.add({
+      targets: bulbasaurImage,
+      y: 500,
+      duration: 1500,
+      ease: "Sine.inOut",
+      yoyo: true,
+      repeat: -1,
+    });
 
-        const logo = this.add.image(400, 70, 'logo');
+    this.tweens.add({
+      targets: venusaurImage,
+      y: 500,
+      duration: 1500,
+      ease: "Sine.inOut",
+      yoyo: true,
+      repeat: -1,
+    });
 
-        this.tweens.add({
-            targets: logo,
-            y: 350,
-            duration: 1500,
-            ease: 'Sine.inOut',
-            yoyo: true,
-            repeat: -1
-        })
-    }
+    this.tweens.add({
+      targets: ivysaurImage,
+      y: 500,
+      duration: 1500,
+      ease: "Sine.inOut",
+      yoyo: true,
+      repeat: -1,
+    });
+  }
 }
 
 const config = {
-    type: Phaser.AUTO,
-    backgroundColor: '#125555',
-    width: 800,
-    height: 600,
-    scene: Demo
+  type: Phaser.AUTO,
+  backgroundColor: "#125555",
+  width: 800,
+  height: 600,
+  scene: Demo,
 };
 
 const game = new Phaser.Game(config);
