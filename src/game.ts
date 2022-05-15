249b7b2c9c0948c916326a555d3e3bb7a50bcaba
Willy Wu <willy@doorstead.com>
2022-05-15 16:31:52 +0800

    Convert game to vertical format, scale to fit
    Also use Phaser's "registry" to manage global data
    
    Links:
    - https://github.com/willywu/pokemon-game/issues/1

--- src/game.ts
+++ src/game.ts
@@ -9,7 +9,7 @@ const getPokemonPaddedNumber = (pokeNum) => {
   return String(pokeNum).padStart(3, "0");
 };
 
-const loadPokemonMetadata = (pokedex, pokeNum) => {
+const loadPokemonData = (pokedex, pokeNum) => {
   // Returns data about the pokemon with give pokeNum
   // Since the pokedex is "index 0"-based, we need to
   // look in the pokedex with pokeNum - 1
@@ -17,8 +17,6 @@ const loadPokemonMetadata = (pokedex, pokeNum) => {
 };
 
 export default class PokemonHomeScene extends Phaser.Scene {
-  localData = {};
-
   constructor() {
     super("pokemonhomescene");
   }
@@ -26,27 +24,29 @@ export default class PokemonHomeScene extends Phaser.Scene {
   preload() {
     this.load.glsl("stars", "assets/starfields.glsl.js");
 
-    this.localData = {};
-    this.localData["pokemonOneNum"] = getPokemonNumber();
-    this.localData["pokemonTwoNum"] = getPokemonNumber();
-    this.localData["pokemonThreeNum"] = getPokemonNumber();
-    this.localData["pokemonFourNum"] = getPokemonNumber();
+    this.registry.set("width", this.game.config.width);
+    this.registry.set("height", this.game.config.height);
+
+    this.registry.set("pokemonOneNum", getPokemonNumber());
+    this.registry.set("pokemonTwoNum", getPokemonNumber());
+    this.registry.set("pokemonThreeNum", getPokemonNumber());
+    this.registry.set("pokemonFourNum", getPokemonNumber());
 
     this.load.image(
       "pokemonOne",
-      `assets/pokemon/thumbnails/${getPokemonPaddedNumber(this.localData["pokemonOneNum"])}.png`
+      `assets/pokemon/thumbnails/${getPokemonPaddedNumber(this.registry.get("pokemonOneNum"))}.png`
     );
     this.load.image(
       "pokemonTwo",
-      `assets/pokemon/thumbnails/${getPokemonPaddedNumber(this.localData["pokemonTwoNum"])}.png`
+      `assets/pokemon/thumbnails/${getPokemonPaddedNumber(this.registry.get("pokemonTwoNum"))}.png`
     );
     this.load.image(
       "pokemonThree",
-      `assets/pokemon/thumbnails/${getPokemonPaddedNumber(this.localData["pokemonThreeNum"])}.png`
+      `assets/pokemon/thumbnails/${getPokemonPaddedNumber(this.registry.get("pokemonThreeNum"))}.png`
     );
     this.load.image(
       "pokemonFour",
-      `assets/pokemon/thumbnails/${getPokemonPaddedNumber(this.localData["pokemonFourNum"])}.png`
+      `assets/pokemon/thumbnails/${getPokemonPaddedNumber(this.registry.get("pokemonFourNum"))}.png`
     );
     this.load.image("button", "assets/button.png");
     this.load.image("button-down", "assets/button-down.png");
@@ -55,44 +55,46 @@ export default class PokemonHomeScene extends Phaser.Scene {
   }
 
   create() {
-    this.add.shader("RGB Shift Field", 0, 0, 800, 600).setOrigin(0);
+    this.add
+      .shader("RGB Shift Field", 0, 0, Number(this.game.config.width), Number(this.game.config.height))
+      .setOrigin(0);
 
     const pokedex = this.cache.json.get("pokedex");
 
-    var pokemonOneNum = this.localData["pokemonOneNum"];
-    var pokemonTwoNum = this.localData["pokemonTwoNum"];
-    var pokemonThreeNum = this.localData["pokemonThreeNum"];
-    var pokemonFourNum = this.localData["pokemonFourNum"];
+    var pokemonOneNum = this.registry.get("pokemonOneNum");
+    var pokemonTwoNum = this.registry.get("pokemonTwoNum");
+    var pokemonThreeNum = this.registry.get("pokemonThreeNum");
+    var pokemonFourNum = this.registry.get("pokemonFourNum");
 
-    const pokemonOne = loadPokemonMetadata(pokedex, pokemonOneNum);
+    const pokemonOne = loadPokemonData(pokedex, pokemonOneNum);
     console.log(pokemonOneNum);
     console.log(pokemonOne.name.english);
 
-    const pokemonTwo = loadPokemonMetadata(pokedex, pokemonTwoNum);
+    const pokemonTwo = loadPokemonData(pokedex, pokemonTwoNum);
     console.log(pokemonTwoNum);
     console.log(pokemonTwo.name.english);
 
-    const pokemonThree = loadPokemonMetadata(pokedex, pokemonThreeNum);
+    const pokemonThree = loadPokemonData(pokedex, pokemonThreeNum);
     console.log(pokemonThreeNum);
     console.log(pokemonThree.name.english);
 
-    const pokemonFour = loadPokemonMetadata(pokedex, pokemonFourNum);
+    const pokemonFour = loadPokemonData(pokedex, pokemonFourNum);
     console.log(pokemonFourNum);
     console.log(pokemonFour.name.english);
 
-    const pokemonOneImage = this.add.image(400, 100, "pokemonOne");
+    const pokemonOneImage = this.add.image(240, 100, "pokemonOne");
 
     const addTextToButton = (button, text) => {
       this.add.text(button.x - 40, button.y - 10, text, {});
     };
 
-    const buttonOneImage = this.add.image(200, 380, "button");
+    const buttonOneImage = this.add.image(240, 440, "button");
     addTextToButton(buttonOneImage, pokemonOne.name.english);
-    const buttonTwoImage = this.add.image(600, 380, "button");
+    const buttonTwoImage = this.add.image(240, 560, "button");
     addTextToButton(buttonTwoImage, pokemonTwo.name.english);
-    const buttonThreeImage = this.add.image(200, 500, "button");
+    const buttonThreeImage = this.add.image(240, 680, "button");
     addTextToButton(buttonThreeImage, pokemonThree.name.english);
-    const buttonFourImage = this.add.image(600, 500, "button");
+    const buttonFourImage = this.add.image(240, 800, "button");
     addTextToButton(buttonFourImage, pokemonFour.name.english);
 
     this.tweens.add({
@@ -115,8 +117,12 @@ export default class PokemonHomeScene extends Phaser.Scene {
 const config = {
   type: Phaser.AUTO,
   backgroundColor: "#125555",
-  width: 800,
-  height: 600,
+  width: 480,
+  height: 900,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
   scene: PokemonHomeScene,
 };
 
